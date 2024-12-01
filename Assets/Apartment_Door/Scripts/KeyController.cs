b26a0f02683ffba8ef6660a6dbef482674b722cf
Charles Wang <lwan256@uwo.ca>
2024-12-01 07:39:05 -0500

    added a door and keys

---
+++ Assets/Apartment_Door/Scripts/KeyController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using TMPro; // Import TextMeshPro namespace
+
+// Script on Drag and drop will create box collider component automatically
+[RequireComponent(typeof(BoxCollider))]
+public class KeyController : MonoBehaviour
+{
+    BoxCollider keyCollider;
+    Rigidbody keyRB;
+    public TextMeshProUGUI txtToDisplay; // Use TextMeshProUGUI instead of Text
+    public DoorController DC;
+
+    /// <summary>
+    /// In case user forgets to uncheck isTrigger in box collider
+    /// This sets them automatically
+    /// </summary>
+    private void Start()
+    {
+        keyCollider = GetComponent<BoxCollider>();
+        keyCollider.isTrigger = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            DC.gotKey = true;
+            txtToDisplay.gameObject.SetActive(true);
+            txtToDisplay.text = "Key Acquired";
+            this.gameObject.SetActive(false);
+        }
+    }
+}
